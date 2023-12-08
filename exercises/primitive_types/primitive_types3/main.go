42f2b1b4e52a69c1742da71db061c5c251825971
Vinícius Viterbo <viniciusvviterbo@protonmail.com>
2023-12-08 00:19:33 -0300

    resolution: primitive_types - completed
    resolution: switch - completed

--- exercises/primitive_types/primitive_types3/main.go
+++ exercises/primitive_types/primitive_types3/main.go
@@ -1,12 +1,12 @@
 // primitive_types3
 // Make me compile!
 
-// I AM NOT DONE
 package main
 
 import "fmt"
 
 func main() {
+  who, country := "Alice", "Wonderland"
 
 	fmt.Printf("Hello, I am %s and live in %s\n", who, country)
 }
